7ec8d9eec73df7398059efabfcfd2f1be9a3871c
JengYoung <young_pallete@naver.com>
2022-03-30 18:42:19 +0900

    :bug: prettier insert error fixed
    prettier에서 printWidth로 인하여 에러 발생하는 현상 수정

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,21 +1,23 @@
 module.exports = {
-  extends: ['stylelint-config-recess-order', "stylelint-config-recommended-vue/scss", "stylelint-config-recommended-vue"],
-  plugins: ["stylelint-prettier", 'stylelint-order'],
+  extends: ['stylelint-config-recess-order', "stylelint-config-standard-scss", "stylelint-config-recommended-vue/scss", "stylelint-config-recommended-vue"],
+  plugins: ["stylelint-prettier", "stylelint-scss", 'stylelint-order'],
   overrides: [
     {
       files: ['**/*.vue'],
       customSyntax: 'postcss-html',
       rules: {
-        "prettier/prettier": true,
+        "prettier/prettier": [true, { "singleQuote": false }],
       },
+      // ignoreFiles: ['**/*.scss']
     },
     {
       files: ['**/*.scss'],
-      customSyntax: 'postcss-html',
+      // customSyntax: 'postcss-html',
       rules: {
-        "prettier/prettier": true,
+        "prettier/prettier": [true, { "singleQuote": false }],
       },
-    }
+      // ignoreFiles: ['**/*.vue']
+    },
   ],
   rules: {
     "no-empty-source": null,
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,7 +11,7 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title> 웹폰트! </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
