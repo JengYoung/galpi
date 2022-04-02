04776792e3aaca107ed2e3ca8055c4959628ac4c
Jaeyoung Hwang <young_pallete@naver.com>
2022-04-02 18:59:40 +0900

    :wrench: add storybook and ts configuration

---
+++ .storybook/main.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/vue3',
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(__dirname, '../src'),
+      '@components': path.join(__dirname, '../src/components/'),
+      '@hooks': path.join(__dirname, '../src/hooks/'),
+    };
+
+    return config;
+  },
+};
