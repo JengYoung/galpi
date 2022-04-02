1e7275f326d9580c5195a86b14865d7e4c666205
Jaeyoung Hwang <young_pallete@naver.com>
2022-04-02 21:06:46 +0900

    :bug: cannot start storybook error fix
    https://storybook.js.org/blog/storybook-for-webpack-5/

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,54 +3,28 @@ module.exports = {
   // This option interrupts the configuration hierarchy at this file
   // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
   root: true,
-
   // https://eslint.vuejs.org/user-guide/#how-to-use-a-custom-parser
   // Must use parserOptions instead of "parser" to allow vue-eslint-parser to keep working
   // `parser: 'vue-eslint-parser'` is already included with any 'plugin:vue/**' config and should be omitted
   parserOptions: {
     parser: require.resolve('@typescript-eslint/parser'),
-    extraFileExtensions: ['.vue'],
+    extraFileExtensions: ['.vue']
   },
-
   env: {
     browser: true,
     es2021: true,
     node: true,
-    'vue/setup-compiler-macros': true,
+    'vue/setup-compiler-macros': true
   },
-
   // Rules order is important, please avoid shuffling them
-  extends: [
-    // Base ESLint recommended rules
-    // 'eslint:recommended',
-
-    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#usage
-    // ESLint typescript rules
-    'plugin:@typescript-eslint/recommended',
-
-    // Uncomment any of the lines below to choose desired strictness,
-    // but leave only one uncommented!
-    // See https://eslint.vuejs.org/rules/#available-rules
-    'plugin:vue/vue3-essential', // Priority A: Essential (Error Prevention)
-    // 'plugin:vue/vue3-strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
-    // 'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
-
-    'airbnb-base',
-    'prettier',
-  ],
-
-  plugins: [
-    // required to apply rules which need type information
-    '@typescript-eslint',
-
-    // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-files
-    // required to lint *.vue files
-    'vue',
-    'prettier',
-  ],
-
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:vue/vue3-essential", "airbnb-base", "prettier", "plugin:storybook/recommended"],
+  plugins: [// required to apply rules which need type information
+  '@typescript-eslint', // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-files
+  // required to lint *.vue files
+  'vue', 'prettier'],
   globals: {
-    ga: 'readonly', // Google Analytics
+    ga: 'readonly',
+    // Google Analytics
     cordova: 'readonly',
     __statics: 'readonly',
     __QUASAR_SSR__: 'readonly',
@@ -59,9 +33,8 @@ module.exports = {
     __QUASAR_SSR_PWA__: 'readonly',
     process: 'readonly',
     Capacitor: 'readonly',
-    chrome: 'readonly',
+    chrome: 'readonly'
   },
-
   // add your custom rules here
   rules: {
     'prettier/prettier': ['error'],
@@ -69,10 +42,8 @@ module.exports = {
     'no-void': 'off',
     'no-nested-ternary': 'off',
     'max-classes-per-file': 'off',
-
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': 'error',
-
     'import/first': 'off',
     'import/namespace': 'error',
     'import/default': 'error',
@@ -81,26 +52,21 @@ module.exports = {
     'import/no-unresolved': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
-
     // The core 'import/named' rules
     // does not work with type definitions
     'import/named': 'off',
-
     'prefer-promise-reject-errors': 'off',
-
-    quotes: ['warn', 'single', { avoidEscape: true }],
-
+    quotes: ['warn', 'single', {
+      avoidEscape: true
+    }],
     // this rule, if on, would require explicit return type on the `render` function
     '@typescript-eslint/explicit-function-return-type': 'off',
-
     // in plain CommonJS modules, you can't use `import foo = require('foo')` to pass this rule, so it has to be disabled
     '@typescript-eslint/no-var-requires': 'off',
-
     // The core 'no-unused-vars' rules (in the eslint:recommeded ruleset)
     // does not work with type definitions
     'no-unused-vars': 'off',
-
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
 };
